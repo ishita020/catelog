e5a623209ec88e81dd9dc33886c06aaca75d4d41
ishita <gangwarnisha21@gmail.com>
2023-03-10 19:16:41 +0530

    day8 compeled

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,15 +12,31 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   String name ="";
   bool changeButton=false;
+  final _formkey = GlobalKey<FormState>();
+  moveToHome(BuildContext context) async {
+    if(_formkey.currentState!.validate()){
+     setState((){
+                  changeButton=true;
+              } );
+             await Future.delayed(Duration(seconds:1));
+             await Navigator.pushNamed(
+              context,MyRoutes.homeRoute);
+              setState(() {
+               changeButton=false;
+              });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
       child:SingleChildScrollView(
+        child: Form(
+         key:_formkey,
       child: Column(
         children:[
           Image.asset(
-            "assets/login.png",
+            "assets/login image 2.png",
             fit: BoxFit.cover,
             ),
             SizedBox(
@@ -37,7 +53,8 @@ class _LoginPageState extends State<LoginPage> {
             height:20.0,
            ),
            Padding(
-            padding: const EdgeInsets.symmetric(vertical:16.0,horizontal: 32.0),
+            padding: const EdgeInsets.symmetric(
+              vertical:16.0,horizontal: 32.0),
          child: Column(
             children: [
                TextFormField(
@@ -45,11 +62,17 @@ class _LoginPageState extends State<LoginPage> {
               hintText: "Enter Username",
               labelText:"Username",
              ),
+             validator: (value) {
+              if(value!.isEmpty){
+                return "Username cannot be empty";
+              }
+             
+              
+              return null;
+             },
              onChanged: (value) {
               name=value;
-              setState(() {
-                 
-              });
+              setState(() {});
              } ,
            ) ,
            TextFormField(
@@ -58,18 +81,26 @@ class _LoginPageState extends State<LoginPage> {
               hintText: "Enter password",
               labelText:"password",
              ) ,
+              validator: (value) {
+              if(value!.isEmpty){
+                return "Password cannot be empty";
+              }
+                else if(value.length< 6){
+                return "password length should be atleast 6";
+              }
+              return null;
+             },
              ),
              SizedBox(
             height:40.0,
            ),
-          InkWell(
-            onTap:() async {
-              setState((){
-                  changeButton=true;
-              } );
-             await Future.delayed(Duration(seconds:1));
-             Navigator.pushNamed(context,MyRoutes.homeRoute);
-            },
+           Material(
+            color:Colors.deepPurple,
+            borderRadius:
+             BorderRadius.circular(changeButton?50:8),
+          child:InkWell(
+           //splashColor: Colors.red,
+            onTap:()=>moveToHome(context),
          child: AnimatedContainer(
           duration:Duration(seconds:1),
             width: changeButton? 50: 150,
@@ -84,18 +115,14 @@ class _LoginPageState extends State<LoginPage> {
             :Text(
               "Login",
             style: TextStyle(
-              color:Colors.white, fontWeight:FontWeight.bold,
+              color:Colors.white,
+               fontWeight:FontWeight.bold,
               fontSize:18
               ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.deepPurple,
-              //shape:changeButton?BoxShape.circle:BoxShape.rectangle,
-             borderRadius: BorderRadius.circular(changeButton?50:8)
-             ),
+            ),            
            ),
           )
-         
+           )
           // ElevatedButton(
             //child: Text("Login"),
             //style: TextButton.styleFrom(minimumSize: Size(150, 50)),
@@ -105,9 +132,11 @@ class _LoginPageState extends State<LoginPage> {
           //  )
             ],
           )
+           
            )
             ],
       )
+        )
       )
     );
   }
